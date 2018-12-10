fbd35f4d57daa799f12c86e1906ebc55ddbfa25c
Raymond Haryanto <raymondharyanto.rh@gmail.com>
2018-12-11 01:07:21 +0700

    finish functionality

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function() {
+  document.getElementById("content").innerHTML = exampleText;
+  window.onscroll = function() {
+    const scrollPos = window.pageYOffset;
+    const docHeight = document.body.offsetHeight;
+    const defaultTimerInMinutes = 2;
+    if(scrollPos >= docHeight/2){
+      const prevDateTime = Date.parse(localStorage.getItem('savedDateTime'));
+      const currentDateTime = new Date();
+      const difference = currentDateTime - prevDateTime;
+      const minutesDifference = Math.floor(difference/1000/60);
+      console.log(minutesDifference);
+      if(minutesDifference >= defaultTimerInMinutes && !(isNaN(minutesDifference))) document.getElementById("slider-ads").classList.remove('hiddenSlider');
+    } else {
+      document.getElementById("slider-ads").classList.add('hiddenSlider');
+    }
+  }
+})();
+
+function closeSticky(e, target){
+  console.log(e, target)
+  switch(target){
+    case "navbar":
+      document.getElementById(target).classList.add('hiddenNavbar');
+      break;
+    case "slider-ads": {
+      var currentDateTime = new Date();
+      localStorage.setItem('savedDateTime', currentDateTime);
+      document.getElementById(target).classList.add('hiddenSlider');
+    }
+      break;
+    default:
+      break;
+  }
+}
